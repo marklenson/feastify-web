a0677c7d1ddab4dab9f8f1e49f69355aa3a9552f
Michael Gift <simiyugift@outlook.com>
2025-01-09 01:45:16 +0300

    Update styling and font

--- src/components/footer/footer.v3.js
+++ src/components/footer/footer.v3.js
@@ -23,31 +23,43 @@ export default function Footer() {
                             }}/>
                         </div>
                         <div className="col-md-3 p-4 text-center text-md-start">
-                            <p className="fs-5 fw-medium" style={{ fontFamily: 'Nunito', color: '#FB9D00' }}>Main Links</p>
+                            <p className="fs-5 fw-medium" style={{ font: 'Nunito', color: '#FB9D00' }}>Main Links</p>
                             <p className="fs-6 fw-normal">
-                                <Link to={eventData} className="text-dark text-decoration-none" rel="noopener noreferrer">Hire Private Chefs</Link>
+                                <Link to={eventData} className="text-dark text-decoration-none"
+                                      rel="noopener noreferrer" style={{ font: 'Nunito'}}>
+                                    Hire Private Chefs
+                                </Link>
                             </p>
                             <p className="fs-6 fw-normal">
-                                <Link to={themeExperience} className="text-dark text-decoration-none" rel="noopener noreferrer">Group Experiences</Link>
+                                <Link to={themeExperience} className="text-dark text-decoration-none"
+                                      rel="noopener noreferrer" style={{ font: 'Nunito'}}>
+                                    Group Experiences
+                                </Link>
                             </p>
                             <p className="fs-6 fw-normal">
-                                <Link to="/contact-us" className="text-dark text-decoration-none" rel="noopener noreferrer">Contact Us</Link>
+                                <Link to="/contact-us" className="text-dark text-decoration-none"
+                                      rel="noopener noreferrer" style={{ font: 'Nunito'}}>
+                                    Contact Us
+                                </Link>
                             </p>
                             <p className="fs-6 fw-normal">
-                                <Link to={chefRegister} className="text-dark text-decoration-none" rel="noopener noreferrer">Chef Register</Link>
+                                <Link to={chefRegister} className="text-dark text-decoration-none"
+                                      rel="noopener noreferrer" style={{ font: 'Nunito'}}>
+                                    Chef Register
+                                </Link>
                             </p>
                         </div>
 
                         <div className="col-md-3 p-4 text-center text-md-start">
-                            <p className="fs-5 fw-medium" style={{ fontFamily: 'Nunito', color: '#FB9D00' }}>Legal</p>
+                            <p className="fs-5 fw-medium" style={{ font: 'Nunito', color: '#FB9D00' }}>Legal</p>
                             <p className="fs-6 fw-normal">
-                                <Link className="text-dark text-decoration-none" to="#">Terms & Conditions</Link>
+                                <Link className="text-dark text-decoration-none" to="#" style={{ font: 'Nunito'}}>Terms & Conditions</Link>
                             </p>
                             <p className="fs-6 fw-normal">
-                                <a className="text-dark text-decoration-none" href="#faq-section" style={{ fontWeight: 380 }}>FAQ</a>
+                                <a className="text-dark text-decoration-none" href="#faq-section" style={{ font: 'Nunito',fontWeight: 380 }}>FAQ</a>
                             </p>
 
-                            <p className="mt-3 fs-5 fw-medium" style={{ fontFamily: 'Nunito', color: '#FB9D00' }}>Payment Accepted</p>
+                            <p className="mt-3 fs-5 fw-medium" style={{ font: 'Nunito', color: '#FB9D00' }}>Payment Accepted</p>
                             <div className="d-flex justify-content-center justify-content-md-start">
                                 <FontAwesomeIcon className="fa-2xl mx-1" icon={faCcVisa} />
                                 <FontAwesomeIcon className="fa-2xl mx-1" icon={faCcMastercard} />
@@ -60,13 +72,13 @@ export default function Footer() {
                     <div className="d-flex flex-column align-items-center mb-3">
                     <div className="d-flex flex-row mb-3" style={{onHover: 'red'}}>
                         <a href="https://www.tiktok.com/@thefeastify" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon className="btn" icon={faTiktok} style={{fontSize:'1.4rem'}} />
+                            <FontAwesomeIcon className="fa-3x btn" icon={faTiktok} style={{fontSize:'1.4rem'}} />
                         </a>
                         <a href="https://www.linkedin.com/company/thefeastify/" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon className="btn" icon={faLinkedin} style={{fontSize:'1.4rem'}} />
+                            <FontAwesomeIcon className="fa-3x btn" icon={faLinkedin} style={{fontSize:'1.4rem'}} />
                         </a>
                         <a href="https://www.instagram.com/thefeastify/" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon className="btn" icon={faInstagram} style={{fontSize:'1.4rem'}} />
+                            <FontAwesomeIcon className="fa-3x btn" icon={faInstagram} style={{fontSize:'1.4rem'}} />
                         </a>
                     </div>
                         <p className="fs-6 fw-light text-center w-100 m-0 text-capitalize" style={{font:'Nunito'}}>
