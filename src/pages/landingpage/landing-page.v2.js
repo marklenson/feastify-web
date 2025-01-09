45b1ef5ec12ec651fcc52d031ea2918f5711ab61
Michael Gift <simiyugift@outlook.com>
2025-01-10 02:55:28 +0300

    Experimental: xxl container

--- src/pages/landingpage/landing-page.v2.js
+++ src/pages/landingpage/landing-page.v2.js
@@ -45,7 +45,7 @@ export default function LandingPage() {
     return (
         <>
 
-            <section className="d-flex flex-column align-items-center" style={{height: 'auto'}}>
+            <section className="container-xxl d-flex flex-column align-items-center" style={{height: 'auto'}}>
                 <p className="mt-4 display-4 fw-bold text-center text-capitalize">
                     Your <span style={{color: '#FB9D00', fontWeight: '550'}}>Event</span>
                     , Our <span style={{color: '#FB9D00', fontWeight: '550'}}>Culinary</span> Magic!
@@ -61,9 +61,9 @@ export default function LandingPage() {
                     Book Event Catering
                 </Link>
 
-                <div className="container mt-4 mb-5">
+                <div className="container-xxl mt-4 mb-5 w-100">
 
-                    <div className="row">
+                    <div className="row mx-auto">
                         <div className="col-md-3 rounded-4 p-1 overflow-hidden">
                             <div className="rounded-4" style={{
                                 backgroundImage: `url(${col1a})`,
@@ -100,7 +100,7 @@ export default function LandingPage() {
                 </div>
             </section>
 
-            <section className="mt-3 mb-5 bg-black rounded-4">
+            <section className="container-xxl mt-3 mb-5 bg-black rounded-4">
                 <div className="row p-3">
                     <div className="col" ref={corporateEventsRef}>
                         <p className="fs-2 fw-medium text-center mb-0" style={{
@@ -145,7 +145,7 @@ export default function LandingPage() {
                 </div>
             </section>
 
-            <section className="mt-5 mb-5">
+            <section className="container-xxl mt-5 mb-5">
                 <p className="fs-1 fw-bold text-center text-capitalize" style={{font: 'Nunito'}}>
                     A taste of the talent we offer
                 </p>
@@ -246,7 +246,7 @@ export default function LandingPage() {
                 </div>
             </section>
 
-            <section className="mt-5 mb-5">
+            <section className="container-xxl mt-5 mb-5">
                 <p className="fs-1 fw-bold text-center text-capitalize" style={{font: 'Nunito'}}>
 
                     Inspired by brands that redefine excellence
@@ -318,7 +318,7 @@ export default function LandingPage() {
                 </div>
             </section>
 
-            <section className="mt-5 mb-5 bg-black rounded-4">
+            <section className="container-xxl mt-5 mb-5 bg-black rounded-4">
                 <div className="d-flex flex-column align-items-center p-5 m-3">
                     <p className="fs-1 fw-bold text-white text-center" style={{font: 'Nunito'}}>
                         Delight in Every Bite, <span style={{color: '#FB9D00'}}>Every Occasion.</span>
@@ -335,7 +335,7 @@ export default function LandingPage() {
             </section>
 
 
-            <section className="mt-4" id="faq-section">
+            <section className="container-xxl mt-4" id="faq-section">
                 <p className="fs-1 text-center text-capitalize fw-medium mt-5" style={{font: 'Nunito'}}>
                     Frequently Asked Questions
                 </p>
@@ -704,7 +704,7 @@ export default function LandingPage() {
                 </div>
             </section>
 
-            <section className="mt-5 mb-5 bg-black rounded-4">
+            <section className="container-xxl mt-5 mb-5 bg-black rounded-4">
                 <div className="d-flex flex-column align-items-center p-5 m-3">
                     <p className="fs-3 text-center text-uppercase" style={{font: 'Nunito', color: '#FB9D00'}}>
                         Let us make your event memorable
